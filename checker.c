ac9942fb6ed854bcd43fbc536553d3f0dbae5d21
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-07-12 18:25:30 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -26,7 +26,7 @@ return ClassifyRange(soc, 20, 80);
 
  int CheckChargeRate(float chargeRate)
  {    
-return ClassifyRange(chargeRate, 0, 0.8);
+return ClassifyRange(chargeRate, 0.8, 1);
 }
   
 int batteryIsOk(float temperature, float soc, float chargeRate) 
@@ -34,9 +34,19 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
  return (ClassifyTemperature(temperature) && CheckStateOfCharge(soc) && CheckChargeRate(chargeRate));
 }
  
-
-int main() 
-{
-  assert(batteryIsOk(25, 70, 0.7));
+int main(){
+/* All Temp, SOC & Charge Rate values are inside range */
+  assert(batteryIsOk(25, 70, 0.9));
+  
+  /* Only Temp value outside range */
+  assert(!batteryIsOk(50, 70, 0.9));
+  
+    /* Only SOC value outside range */
+  assert(!batteryIsOk(30, 85, 0.9));
+  
+    /* Only Charge Rate outside range */
+  assert(!batteryIsOk(40, 70, 0.7));
+  
+  /* All Temp, SOC & Charge Rate values are outside range */
   assert(!batteryIsOk(50, 85, 0));
 }
