dadd2f3d047bc424debad5392ebd7bbd7fe3501d
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-07-12 18:14:25 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -2,7 +2,8 @@
 #define E_OK 1
 #define E_NOT_OK 0
 
-int CheckTemp(float temperature);
+int ClassifyRange(float CurrentValue, int MinRange, int MaxRange);
+int ClassifyTemperature(float temperature);
 int CheckStateOfCharge(float soc);
 int CheckChargeRate(float chargeRate);
 
